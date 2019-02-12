ce83b916c162cd065ebd45116efac2a7d1a8f0d6
Carlos Huerta <carlos.huertaso@udlap.mx>
2019-02-12 14:45:02 +0100

    some initial changes to calculate the LOOCV

--- assignment1.R
+++ assignment1.R
@@ -1,3 +1,7 @@
+# Imports
+# install via cran: "install.packages("pracma", repos="http://R-Forge.R-project.org")"
+library('pracma')
+
 print('Im working!')
 # Clear the Environment Variables
 rm(list = ls())
@@ -29,14 +33,56 @@ Y = X1 - X2 + epsilons
 # Set up the lamda parameter
 lamda = 0.5
 
-# lamda matrix
-lamdaMatrix = lamda * diag(2)
+# Func that calculates the ridge parameters
+get_ridge_estimators = function(data, Y, lamda){
+  # lamda matrix
+  lamdaMatrix = lamda * diag(2)
+  
+  # Calculate t(X) * X
+  # Note solve(arg) calculates the inverse of a sqr matrix
+  betas_ridge = (solve((t(data) %*% data) + lamdaMatrix) %*% t(data)) %*% Y
+  
+  # Return
+  return(betas_ridge)
+}
 
-# Calculate t(X) * X
-# Note solve(arg) calculates the inverse of a sqr matrix
-betas_ridge = (solve((t(data) %*% data) + lamdaMatrix) %*% t(data)) %*% Y
+# Func that calculates the MSE
+calculate_mse = function(betas_ridge, data, Y){
+  # Calculate Average squared error of the estimators
+  Y_pred = data %*% betas_ridge
+  errors_vector = (Y - Y_pred) ^ 2
+  MSE = sum(errors_vector)/10
+  return(MSE)
+}
 
-# Calculate Average squared error of the estimators
-Y_pred = data %*% betas_ridge
-errors_vector = (Y - Y_pred) ^ 2
-MSE = sum(errors_vector)/10
+# Generate a linspace for the lamdas
+lamdas_vector = linspace(0,1,9)
+
+# Grid Search over the lamdas linspace
+for (i in 1:length(lamdas_vector)){
+  # Get the current lamda from the linspace
+  current_lamda = lamdas_vector[i]
+  
+  # Declare a empty vector where the X-val errors are going to be saved
+  current_mse_vector = numeric()
+  # Get the MSE error over LOOCV loop 
+  for (j in 1:10){
+    # Remove the ith example
+    current_data = data[-j,1:2]
+    current_Y    = Y[-j]
+    
+    # Generate the estimators
+    current_betas = get_ridge_estimators(current_data, current_Y, current_lamda)
+    current_mse   = calculate_mse(current_betas, current_data, current_Y)
+    
+    # Append to the error vector for the current lamda
+    current_mse_vector = append(current_mse_vector, current_mse)
+  }
+  # Reduce the error vector
+  avg_error = sum(current_mse_vector)/10
+  
+  # Print Results, NOTE: you need to use cat() because sprintf doesnt print in non-interactive mode
+  cat(sprintf("The current average MSE is: %#.5f for the lamda value: %#.2f \n", avg_error, current_lamda))
+  
+}
+
