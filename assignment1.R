f73483558c68cdfb29c29ebde6e80ed67239c042
Carlos Huerta <carlos.huertaso@udlap.mx>
2019-02-12 15:58:16 +0100

    implemented LOOCV and Grid search with fixed tau parameter

--- assignment1.R
+++ assignment1.R
@@ -2,7 +2,6 @@
 # install via cran: "install.packages("pracma", repos="http://R-Forge.R-project.org")"
 library('pracma')
 
-print('Im working!')
 # Clear the Environment Variables
 rm(list = ls())
 
@@ -16,7 +15,7 @@ X1 = rnorm(10, 0, 1);
 epsilons = rnorm(10,0,1)
 
 # Specify the tau parameter
-tau = 0.5
+tau = 0.9
 
 # The U parameter for the generation of X2
 U = rnorm(10,0,tau^2)
@@ -49,16 +48,19 @@ get_ridge_estimators = function(data, Y, lamda){
 # Func that calculates the MSE
 calculate_mse = function(betas_ridge, data, Y){
   # Calculate Average squared error of the estimators
-  Y_pred = data %*% betas_ridge
+  Y_pred        = data %*% betas_ridge
   errors_vector = (Y - Y_pred) ^ 2
-  MSE = sum(errors_vector)/10
+  MSE           = sum(errors_vector) / length(Y)
   return(MSE)
 }
 
 # Generate a linspace for the lamdas
-lamdas_vector = linspace(0,1,9)
+lamdas_vector = linspace(0,100,1000)
 
 # Grid Search over the lamdas linspace
+best_lamda = numeric()
+best_mse   = 10000
+
 for (i in 1:length(lamdas_vector)){
   # Get the current lamda from the linspace
   current_lamda = lamdas_vector[i]
@@ -66,23 +68,35 @@ for (i in 1:length(lamdas_vector)){
   # Declare a empty vector where the X-val errors are going to be saved
   current_mse_vector = numeric()
   # Get the MSE error over LOOCV loop 
-  for (j in 1:10){
+  for (j in 1:(dim(data)[1])){
     # Remove the ith example
     current_data = data[-j,1:2]
     current_Y    = Y[-j]
     
     # Generate the estimators
     current_betas = get_ridge_estimators(current_data, current_Y, current_lamda)
-    current_mse   = calculate_mse(current_betas, current_data, current_Y)
+    # Test the error on the point you leaved out
+    current_point = data[j,1:2]
+    point_Y       = Y[j]
+    current_error = calculate_mse(current_betas, current_point, point_Y)
     
     # Append to the error vector for the current lamda
-    current_mse_vector = append(current_mse_vector, current_mse)
+    current_mse_vector = append(current_mse_vector, current_error)
   }
   # Reduce the error vector
   avg_error = sum(current_mse_vector)/10
   
-  # Print Results, NOTE: you need to use cat() because sprintf doesnt print in non-interactive mode
-  cat(sprintf("The current average MSE is: %#.5f for the lamda value: %#.2f \n", avg_error, current_lamda))
+  if (i %% 100 == 0) {
+    # Print Results, NOTE: you need to use cat() because sprintf doesnt print in non-interactive mode
+    cat(sprintf("The current average MSE is: %#.5f for the lamda value: %#.2f \n", avg_error, current_lamda))
+  }
   
+  # Best lamda check
+  if (avg_error < best_mse) {
+    best_mse   = avg_error
+    best_lamda = current_lamda
+  }
 }
 
+sprintf("The best lamda was: %#.2f with an MSE of %#.5f", best_lamda, best_mse)
+# plot_ly(x=X1, y=X2, z=Y, type="scatter3d", mode="markers")
